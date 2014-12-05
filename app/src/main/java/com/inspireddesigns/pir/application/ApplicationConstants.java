742805577c6c243ca7ccabfc3a56bf0d226af102
blsiege <blsiege@gmail.com>
2014-12-05 14:00:12 -0600

    Created NavigationDrawerAdapter used to display navigation drawer items based on the type of user that is logged in

---
+++ app/src/main/java/com/inspireddesigns/pir/application/ApplicationConstants.java
@@ -0,0 +1,24 @@
+package com.inspireddesigns.pir.application;
+
+/**
+ * Contains constants used throughout the application
+ *
+ * Created by blsiege on 12/5/14.
+ */
+public class ApplicationConstants {
+
+    /**
+     * Indicates that the user is an admin
+     */
+    public static final int USER_ADMIN = 1;
+
+    /**
+     * Indicates that the user is a parent
+     */
+    public static final int USER_PARENT = 2;
+
+    /**
+     * Indicates that the user is a front desk employee
+     */
+    public static final int USER_FRONT_DESK = 3;
+}
